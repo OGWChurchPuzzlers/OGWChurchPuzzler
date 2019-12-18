35ebe251fbfedbb84e94d5ab2f35772ba6b065d3
Karlheinz Reinhardt <reinhardt.karlheinz@gmail.com>
2019-12-18 12:44:40 +0100

    Touch input test

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -13,6 +13,27 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+                // Create a particle if hit
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    //                    Instantiate(particle, transform.position, transform.rotation);
+                    // the object identified by hit.transform was clicked
+                    // do whatever you want
+                    Debug.Log("#### object touched + item hit");
+                    Debug.Log(hit.transform.gameObject);
+                    GameObject.FindObjectOfType<CharacterController>().AttachObject();
+                }
+            }
+        }
+        /**
         if (Input.GetMouseButtonDown(0))
         { // if left button pressed...
             Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
@@ -25,5 +46,6 @@ public class CameraController : MonoBehaviour
                 GameObject.FindObjectOfType<CharacterController>().AttachObject();
             }
         }
+    **/
     }
 }
