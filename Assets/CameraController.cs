548d9bcefdb5d8a1d9d7e7da44730baade0c3297
reinhardt <karlheinz.reinhardt@cip.ifi.lmu.de>
2019-12-18 20:41:02 +0100

    Touch Mode toggle

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,48 +4,56 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] public bool Debug_touchModeEnabled = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Input.touchCount; ++i)
+        if (Debug_touchModeEnabled)
         {
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
-            {
-                // Construct a ray from the current touch coordinates
-                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+            // ## Touch Mode
 
-                // Create a particle if hit
+            for (int i = 0; i < Input.touchCount; ++i)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    // Construct a ray from the current touch coordinates
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+                    // Create a particle if hit
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        //                    Instantiate(particle, transform.position, transform.rotation);
+                        // the object identified by hit.transform was clicked
+                        // do whatever you want
+                        Debug.Log("#### object touched + item hit");
+                        Debug.Log(hit.transform.gameObject);
+                        GameObject.FindObjectOfType<CharacterController>().AttachObject();
+                    }
+                }
+            }
+        }
+        else
+        {
+            // ## Keyboard Mode
+            if (Input.GetMouseButtonDown(0))
+            { // if left button pressed...
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
                 if (Physics.Raycast(ray, out hit))
                 {
-                    //                    Instantiate(particle, transform.position, transform.rotation);
                     // the object identified by hit.transform was clicked
                     // do whatever you want
-                    Debug.Log("#### object touched + item hit");
                     Debug.Log(hit.transform.gameObject);
                     GameObject.FindObjectOfType<CharacterController>().AttachObject();
                 }
             }
         }
-        /**
-        if (Input.GetMouseButtonDown(0))
-        { // if left button pressed...
-            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                // the object identified by hit.transform was clicked
-                // do whatever you want
-                Debug.Log(hit.transform.gameObject);
-                GameObject.FindObjectOfType<CharacterController>().AttachObject();
-            }
-        }
-    **/
     }
 }
