32a1d46cfc306a00996fd7262ec67594bfece82d
reinhardt <karlheinz.reinhardt@cip.ifi.lmu.de>
2020-01-02 02:33:04 +0100

    Bugfix Schalter und Hebel rotation falsch nach drehung von Objekt (lag an Mix von Quaternionen)

--- Assets/Controllers/Hebel.cs
+++ Assets/Controllers/Hebel.cs
@@ -6,34 +6,34 @@ public class Hebel : Interactable
 {
     public bool inOnPosition = false;
     [SerializeField] private GameObject hebel;
-    private Quaternion intialRoation;
+    //private Quaternion intialRoation;
 
     public override void ExecuteInteractionAnimation()
     {
         if (inOnPosition)
         {
             Debug.Log("## Hebel On");
-            //hebel.transform.Rotate(0,45, 0, Space.Self);
-            hebel.transform.localRotation = intialRoation;
+            hebel.transform.Rotate(0, -45, 0, Space.Self);
+            //hebel.transform.localRotation = intialRoation;
             inOnPosition = false;
         }
         else
         {
             Debug.Log("## Hebel Off");
-            // hebel.transform.Rotate(0,-45, 0, Space.Self);
-            hebel.transform.rotation = intialRoation*Quaternion.Euler(0,45,0);
+            hebel.transform.Rotate(0,45, 0, Space.Self);
+            //hebel.transform.localRotation = Quaternion.Euler(0,45,0);
             inOnPosition = true;
         }
     }
 
     public override void HandleInteractableReset()
     {
-        hebel.transform.localRotation = intialRoation;
+        //hebel.transform.localRotation = intialRoation;
     }
 
     public override void InitializeInteractable()
     {
-        intialRoation = hebel.transform.localRotation;
+        //intialRoation = hebel.transform.localRotation;
     }
 
     public override void UpdateInteractable()
--- Assets/Controllers/Lichtschalter.cs
+++ Assets/Controllers/Lichtschalter.cs
@@ -6,32 +6,33 @@ public class Lichtschalter : Interactable
 {
     public bool inOnPosition = false;
     [SerializeField] private GameObject hebel;
-    private Quaternion intialRoation;
+    //private Quaternion intialRoation;
 
     public override void ExecuteInteractionAnimation()
     {
         if (inOnPosition)
         {
             Debug.Log("## Lichtschalter On");
-            hebel.transform.localRotation = intialRoation;
+            hebel.transform.Rotate(0, 90, 0, Space.Self);
+            //hebel.transform.localRotation = intialRoation;
             inOnPosition = false;
         }
         else
         {
             Debug.Log("## Lichtschalter Off");
-            hebel.transform.rotation = intialRoation * Quaternion.Euler(0, -90, 0);
+            hebel.transform.Rotate(0, -90, 0,Space.Self);
             inOnPosition = true;
         }
     }
 
     public override void HandleInteractableReset()
     {
-        hebel.transform.localRotation = intialRoation;
+        //hebel.transform.localRotation = intialRoation;
     }
 
     public override void InitializeInteractable()
     {
-        intialRoation = hebel.transform.localRotation;
+        //intialRoation = hebel.transform.localRotation;
     }
 
     public override void UpdateInteractable()
