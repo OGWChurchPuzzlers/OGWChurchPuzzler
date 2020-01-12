2daac68c06df03ab69abced874ab43e8c03fddac
Karlheinz Reinhardt <reinhardt.karlheinz@gmail.com>
2020-01-12 16:33:59 +0100

    Vobereitung DecoupledInputManager für merge in Game

---
+++ Assets/Controllers/DecoupledInputManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum GameInputMode
+{
+    Keyboard,
+    Touch_1,
+}
+
+public class DecoupledInputManager : MonoBehaviour
+{
+    [SerializeField] bool blockControl = false;
+    [SerializeField] GameInputMode mode = GameInputMode.Keyboard;
+    [SerializeField] GameObject JoystickAsset;
+    [SerializeField] Joystick joystick
+    {
+        get
+        {
+            return JoystickAsset.GetComponent<Joystick>();
+        }
+    }
+
+    public Vector2 touchInputVector = new Vector2(0, 0);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // TODO nachforschen wo/wann am besten refresh stattfinden sollte. erst bei update() oder schon früher? 
+        refreshInputVector();
+    }
+   
+
+    private void refreshInputVector()
+    {
+        touchInputVector = new Vector2(0, 0);
+        switch (mode)
+        {
+            case GameInputMode.Keyboard:
+                touchInputVector.x = Input.GetAxis("Horizontal");
+                touchInputVector.y = Input.GetAxis("Vertical");
+                break;
+            case GameInputMode.Touch_1:
+                touchInputVector = joystick.Direction;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public bool IsInteractionKeyPressed()
+    {
+        //TODO
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Vector2 GetInputVector()
+    {
+        if (blockControl)
+        {
+            return Vector2.zero;
+        }
+        return touchInputVector;
+    }
+
+    public float GetAxis(string a_axis)
+    {
+        if (blockControl)
+        {
+            return 0f;
+        }
+
+        switch (a_axis)
+        {
+            case "Horizontal":
+                return touchInputVector.x;
+            case "Vertical":
+                return touchInputVector.y;
+            default:
+                break;
+        }
+        return 0f;
+    }
+}
+
---
+++ Assets/Controllers/JoystickForwardSnap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickForwardSnap : Joystick
+{
+    [SerializeField] private float m_Angle = 0f;
+    public SegmentedJoystickMode mode = SegmentedJoystickMode.DPad4;
+
+    // HACK
+    protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        m_Angle = Vector2.SignedAngle(normalised, new Vector2(0, 1));
+
+                float forward_lockAngle = 7.5f; // 5
+                if (inBetween(m_Angle, 0, forward_lockAngle) || m_Angle == 0 || inBetween(m_Angle, -forward_lockAngle, 0))
+                {
+                    //Debug.Log("T");
+                    normalised = new Vector2(0, 1);
+                }
+
+                //Debug.Log("angle: " +m_Angle);
+                //Debug.Log("magnitude: " + magnitude);
+                if (magnitude < 1)
+                {
+                    normalised = normalised * magnitude;
+                }
+
+        base.HandleInput(2.0f, normalised, radius, cam);
+        bool inBetween(float a, float c, float d)
+        {
+            float max = System.Math.Max(c, d);
+            float min = System.Math.Min(c, d);
+
+            return a < max && a > min;
+        }
+    }
+}
