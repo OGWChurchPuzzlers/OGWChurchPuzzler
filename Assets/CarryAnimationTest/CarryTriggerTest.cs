5902b2b007bb512e2fbd8532a4b94c42f3771aca
Karlheinz Reinhardt <reinhardt.karlheinz@gmail.com>
2020-01-15 23:48:47 +0100

    Carry Animation: Variante 1 - Hände ausgestreckt

---
+++ Assets/CarryAnimationTest/CarryTriggerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryTriggerTest : MonoBehaviour
+{
+    [SerializeField] Animator characterAnimator;
+    [SerializeField] string trigger;
+    [SerializeField] bool state = false;
+
+    public void SetAnimation(bool b)
+    {
+        state = b;
+        characterAnimator.SetBool(trigger,state);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
