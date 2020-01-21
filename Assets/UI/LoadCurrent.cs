372f4b9f0291877046c9dfdfe0651661f1da6964
lilefisch <56924393+lilefisch@users.noreply.github.com>
2020-01-21 09:26:46 +0100

    Add files via upload
    Files for UI

---
+++ Assets/UI/LoadCurrent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadCurrent : MonoBehaviour
+{
+	
+	public void LoadCurrentScene()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
