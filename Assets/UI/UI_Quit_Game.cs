d0b4c12e5086df3b5058ebd6023a90501d614b74
Karlheinz Reinhardt <reinhardt.karlheinz@gmail.com>
2020-01-22 09:42:30 +0100

    Quit Button

---
+++ Assets/UI/UI_Quit_Game.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class UI_Quit_Game : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game. Goodbye");
+        // save any game data here
+#if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+     EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
